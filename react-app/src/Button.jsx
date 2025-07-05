0a5b272a72ff6a6d1ac79f6b05efdacfbb087cd6
safwancp-dev <safwancp956@gmail.com>
2025-07-05 15:22:11 +0530

    updated

--- react-app/src/Button.jsx
+++ react-app/src/Button.jsx
@@ -1,7 +1,15 @@
 
-
 function Button(){
-    return(<button className="button">Click me</button>)
+    const styles={
+        backgroundColor:" hsl(200, 100%, 50%)",
+        color: "white",
+        padding: "10px 20px",
+        borderRadius:" 5px",
+        border: "none",
+        cursor: "pointer",
+
+    }
+    return(<button style={styles}>Click me</button>)
 }
 
 
