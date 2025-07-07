dfe7d92f129d98c7039ef1baf4e90a31143edb00
safwancp-dev <safwancp956@gmail.com>
2025-07-07 12:06:56 +0530

    #	modified:   react-app/package-lock.json

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,6 +1,14 @@
-import Button from './Button.jsx'
+import Student from './Student.jsx'
 function App() {
-  return (<Button/>);
+  return(
+    <>
+    <Student name="safwan" age={18} isStudent={true}/>
+    <Student name="adil" age={19} isStudent={false}/>
+    <Student name="rabeeh" age={25} isStudent={false}/>
+    <Student name="sinan" age={31} isStudent={true}/>
+    <Student  />
+    </>
+  )
 }
 
 export default App
